edb6d5b19cf1f5da2b3f6f5bd0efd6e4eacb1ffe
Kenji Saito <ken-yo@mbr.nifty.com>
2025-01-02 23:20:45 +0900

    Add the show username

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { JSX } from 'react';
+import { Show } from './flow';
 import { Auth } from './auth/Auth';
+import useAuth from './auth/hooks';
+
 import '@aws-amplify/ui-react/styles.css';
 
 /**
@@ -8,12 +11,22 @@ import '@aws-amplify/ui-react/styles.css';
  * @returns {JSX.Element} アプリケーションのメインUIにAuthコンポーネントを表示
  */
 function App(): JSX.Element {
+  const { user, isSignedIn } = useAuth();
 
   return (
-    <main>
+    <><main>
       {/* 認証コンポーネントを表示 */}
       <Auth />
-    </main>
+      <Show when={isSignedIn}>
+        <div>
+          <h1>User</h1>
+          <div>
+            <p>Username: {user?.username}</p>
+          </div>
+        </div>
+      </Show>
+    </main >
+    </>
   );
 }
 
