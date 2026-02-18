b7c4546754868109d3744409fa6bba9c8a19d4de
Kenji Saito <ken-yo@mbr.nifty.com>
2026-02-18 21:33:02 +0900

    Bumps node modules

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,23 +4,23 @@ import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+// @ts-expect-error ignore errors
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import importPlugin from 'eslint-plugin-import';
+// @ts-expect-error ignore errors
 import pluginPromise from 'eslint-plugin-promise';
-import reactRefresh from "eslint-plugin-react-refresh";
+// eslint-disable-next-line import/no-named-as-default
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 import { configs, parser } from 'typescript-eslint';
-import { FlatCompat } from '@eslint/eslintrc';
 
 import { includeIgnoreFile } from '@eslint/compat';
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, "./.gitignore");
-
-const compat = new FlatCompat();
+const gitignorePath = path.resolve(__dirname, './.gitignore');
 
 export default defineConfig(
   includeIgnoreFile(gitignorePath),
@@ -38,7 +38,6 @@ export default defineConfig(
   eslint.configs.recommended,
   ...configs.strict,
   ...configs.stylistic,
-  // @ts-ignore
   pluginPromise.configs['flat/recommended'],
   reactHooks.configs.flat.recommended,
   reactRefresh.configs.recommended,
